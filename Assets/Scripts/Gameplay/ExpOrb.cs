8560d1288879b80ab29ad40f8ad1831491dfb623
jongwo0k <pygudo@naver.com>
2026-01-29 01:30:57 +0900

    Refactor remove redundant code

--- Assets/Scripts/Gameplay/ExpOrb.cs
+++ Assets/Scripts/Gameplay/ExpOrb.cs
@@ -4,14 +4,4 @@ public class ExpOrb : MonoBehaviour
 {
     // 1개당 경험치 증가량
     [SerializeField] public float expValue = 10f;
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            PlayerController player = other.GetComponent<PlayerController>();
-            player.GetXP(expValue);
-            Destroy(gameObject);
-        }
-    }
 }
